0466693df8eb64291c8e8af53a0f409ed6102e7b
piyushgoyal356 <piyushgoyal356@gmail.com>
2021-02-14 21:45:10 +0530

    capture from webcam

---
+++ face_recognition/face_recognition.py
@@ -0,0 +1,20 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+
+    ret,frame = cap.read()
+    
+    if ret==False:
+        continue
+
+    cv2.imshow("Video capture",frame)
+
+    key_pressed = cv2.waitKey(1) & 0xFF
+    if key_pressed == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
